a9b045e488c7dab9cf5fa142d01a6fabbae68ec3
vnkoroll <vn_korol@mail.ru>
2015-03-25 19:56:22 +0200

    First main Java edit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+      int  x=256;
+      int y=3150;
+	System.out.println(12+20);
+        System.out.println("Sum of 256 and 3150 is " + (x+y));
+        System.out.println("180/30 is "+ (180/30));
+
+        boolean t=true;
+        boolean g=false;
+
+        //Массив
+        int[] h={10,0,0,21,-5,38};
+        System.out.println(h[0]+" "+ h[1]+" "+h[2]+" "+h[3]+" "+h[4]+" "+h[5]);
+
+        String [] j2 = {"a","b", "c"};
+        String [] j3 = {"a","d", "c"};
+        System.out.println (j2[0]+"-"+j3[0]+", "+j2[1]+"-"+j3[1]+" , "+j2[2]+"-"+j3[2]);
+    }
+}
