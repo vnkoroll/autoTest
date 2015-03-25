38695d63793e1cf845be6753d96e31102c3e5312
vnkoroll <vn_korol@mail.ru>
2015-03-25 21:02:29 +0200

    First main Java edit

---
+++ src/com/company/main2.java
@@ -0,0 +1,35 @@
+package com.company;
+
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+import javax.xml.soap.SOAPPart;
+
+/**
+ * Created by Admin on 25.03.15.
+ */
+public class main2 {
+    public static void main(String[] args) {
+
+       /* if (args[0].equals("Hello"))
+        {
+            System.out.println("Hello");
+        }
+
+        else {
+            System.out.println("Good bay");
+        }
+*/
+
+        if (args[0].equals("Hello") && args[1].equals("Class"))
+        {
+                System.out.println("Succes");
+        }
+
+        else {
+            System.out.println("Good bay");
+        }
+
+
+
+    }
+}
