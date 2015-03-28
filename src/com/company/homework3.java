384ed342849217e90cfab05657f53ab440dd5532
vnkoroll <Qatester2014>
2015-03-28 14:15:20 +0200

    homeWork3

---
+++ src/com/company/homework3.java
@@ -0,0 +1,17 @@
+package com.company;
+
+/**
+ * Created by Max on 28.03.2015.
+ */
+public class homework3 {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+        System.out.println(a);
+        // 10
+
+        System.out.println(a + b);
+// 30
+    }
+}
