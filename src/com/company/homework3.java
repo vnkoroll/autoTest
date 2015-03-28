1e17d4851a3075c3e4bff1909f33d465f5f07bb1
vnkoroll <Qatester2014>
2015-03-28 14:18:19 +0200

    homeWork3

--- src/com/company/homework3.java
+++ src/com/company/homework3.java
@@ -13,5 +13,27 @@ public class homework3 {
 
         System.out.println(a + b);
 // 30
+        String s = "Hello class";
+        System.out.println(s);
+// Hello class
+        String c = "15";
+        System.out.println(c);
+// 15
+        System.out.println(s + c);
+// Hello class15
+        if (a < b) {
+            System.out.println("a less then b");
+        }
+// a less then b
+        if (a > b) {
+            System.out.println("a more then b");
+        } else if (a == b) {
+            System.out.println("a equals b");
+        } else {
+            System.out.println("conditions not met");
+        }
+
+
+
     }
 }
