1512e2b0dba3ee609d2ccf5731d821fe057ac045
vnkoroll <vn_korol@mail.ru>
2015-04-15 21:16:02 +0300

    http://oxogamestudio.com/

--- src/com/company/JUnit/oxogamestudio.java
+++ src/com/company/JUnit/oxogamestudio.java
@@ -1,21 +1,22 @@
 package com.company.JUnit;
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+        import org.junit.After;
+        import org.junit.Assert;
+        import org.junit.Before;
+        import org.junit.Test;
+        import org.junit.runner.RunWith;
+        import org.junit.runners.JUnit4;
+        import org.openqa.selenium.By;
+        import org.openqa.selenium.Keys;
+        import org.openqa.selenium.WebDriver;
+        import org.openqa.selenium.WebElement;
+        import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
- * Created by Admin on 08.04.15.
+ * Created by Admin on 15.04.15.
  */
 @RunWith(JUnit4.class) // for import JUnit для запуска функций как тесты
-public class AngelNetJUnit {
-public WebDriver dr; // for @After пишем эту строку
+public class oxogamestudio {
+    public WebDriver dr; // for @After пишем эту строку
 
     //прописываем функции
     //void ничего не возвращает
@@ -32,8 +33,6 @@ public WebDriver dr; // for @After пишем эту строку
         return getPass;
     }
 
-
-
     @Before
     public void before ()
     {
@@ -41,11 +40,11 @@ public WebDriver dr; // for @After пишем эту строку
                 "C:/Automation/chromedriver.exe"); //прописывает параметры и путь к Хроме драйверу.
         dr = new ChromeDriver(); //запускает драйвер
         dr.manage().window().maximize();//на весь экран открыть браузер
-        dr.get("http://angel.net/~nic/passwd.current.html");
+        dr.get("http://oxogamestudio.com/passwd.current2.htm");
     }
 
     @Test
-        public void test1 (){
+    public void test1 (){
 
         //String a = "12345";
         //String b = "vnkoroll@gmai.com";
@@ -56,7 +55,7 @@ public WebDriver dr; // for @After пишем эту строку
         String expectedResult = "DY/QHKxH7ggdW@1a";
         //String D =  dr.findElement(By.name("password")).getAttribute("value");
         Assert.assertEquals(expectedResult, getPassword());
-}
+    }
     @Test
     public void test2 (){
 
@@ -70,26 +69,26 @@ public WebDriver dr; // for @After пишем эту строку
         //String D =  dr.findElement(By.name("password")).getAttribute("value");
         Assert.assertEquals(expectedResult, getPassword());
     }
-@Test
-public void test3 (){
+    @Test
+    public void test3 (){
         setName ("");
         //dr.findElement(By.name("site")).sendKeys(Keys.ENTER);
         String expectedResult = "BaefBs8/Z/cm2@1a";
         //String D =  dr.findElement(By.name("password")).getAttribute("value");
         Assert.assertEquals(expectedResult, getPassword());
     }
- @Test
- public void test4 (){
-     //String a = "1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as";
-     setMaster("1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as");
-     setName("1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as");
-     //String b = "1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as";
-     //dr.findElement (By.name("master")).sendKeys(a);
-     //dr.findElement(By.name("site")).sendKeys(b, Keys.ENTER);
-     String expectedResult = "+Qv8Z2wl9tTb9@1a";
-     //String D =  dr.findElement(By.name("password")).getAttribute("value");
-     Assert.assertEquals(expectedResult, getPassword());
- }
+    @Test
+    public void test4 (){
+        //String a = "1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as";
+        setMaster("1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as");
+        setName("1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as");
+        //String b = "1234565656565656656565656548878888848545454545a4s5a5s5a45a4s5a4d55as5as5a5s5as";
+        //dr.findElement (By.name("master")).sendKeys(a);
+        //dr.findElement(By.name("site")).sendKeys(b, Keys.ENTER);
+        String expectedResult = "+Qv8Z2wl9tTb9@1a";
+        //String D =  dr.findElement(By.name("password")).getAttribute("value");
+        Assert.assertEquals(expectedResult, getPassword());
+    }
 
     @Test
     public void test5 (){
@@ -115,9 +114,9 @@ public void test3 (){
     }
 
     @After //чтобы браузер закрылся независимо от результатов
-public void quitBrowser () {
-dr.quit();
-}
+    public void quitBrowser () {
+        dr.quit();
+    }
 
 
 
