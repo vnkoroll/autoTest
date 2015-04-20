e5e754df7017af64b797262a76f41ca7a007ab8f
vnkoroll <vn_korol@mail.ru>
2015-04-20 21:11:42 +0300

    xpatch

---
+++ src/com/company/XPatch/xpatch.java
@@ -0,0 +1,19 @@
+package com.company.XPatch;
+
+/**
+ * Created by Admin on 20.04.15.
+ */
+public class xpatch {
+/**
+    // - искать везде
+    / - искатьна уровень ниже
+    //book[@author='Tolstoy']   ищем по тегу
+    //book[2]   номер попорядку
+    //*[@id='name']    любой елеменнт где есть id='name'
+    //pages[text()='999']  найти где есть текст
+    /..  подняться на уровень выше
+    //book[count(name)=2] найти книгу у которой количество name=2
+    //title[text()='hi']/following::shell[1]
+ */
+
+}
